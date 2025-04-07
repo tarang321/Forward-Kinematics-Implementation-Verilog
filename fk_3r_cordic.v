fb95f601f97b8c40646e435d04fe468f516965df
Tarang Srivas <37020491+tarang321@users.noreply.github.com>
2025-04-07 22:32:40 +0530

    Update fk_3r_cordic.v

--- fk_3r_cordic.v
+++ fk_3r_cordic.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.04.2025 21:44:24
-// Design Name: 
-// Module Name: fk_3r_cordic
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module fk_3r_cordic (
     input clk,
     input rst,
